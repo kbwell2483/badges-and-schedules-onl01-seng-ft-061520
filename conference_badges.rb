b4a14a30871efcaf171a411e90cbf04ef4c9a139
Kelly Blackwell <kellylenorablackwell@gmail.com>
2020-06-23 05:12:49 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+badge_maker("Arel")
+end
+
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator
+speakers.each do |names|
+  puts "Hello, my name is #{names}."
+end
+end
+
+
+
---
+++ for_run.rb
@@ -0,0 +1,4 @@
+rooms_numbers = ["#1", "#2", "#3", "#4", "#5", "#6", "#7"]
+def assign_rooms 
+  puts "Hello, ___! You'll be assigned to room ___"
+end
