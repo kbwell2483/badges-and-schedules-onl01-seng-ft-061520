255ece29d1d685821110a5019a78fa58bce44fa4
Kelly Blackwell <kellylenorablackwell@gmail.com>
2020-06-23 18:40:11 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,13 @@
-
-
-attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-
-def batch_badge_creator
-  attendees.each do |name|
+def badge_maker(name)
   "Hello, my name is #{name}."
 end
 end
 
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator
+    speaker.each do |badges|
+    "Hello, my name is #{badges}."
+end
+end
 
